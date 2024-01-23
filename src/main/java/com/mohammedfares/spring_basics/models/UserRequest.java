45fdbbe79f370f084eb4f3dcab0eadecbcf472da
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-23 12:23:57 +0100

    create the UserRequest

--- src/main/java/com/mohammedfares/spring_basics/models/UserRequest.java
+++ src/main/java/com/mohammedfares/spring_basics/models/UserRequest.java
@@ -2,14 +2,22 @@ package com.mohammedfares.spring_basics.models;
 
 public class UserRequest {
 	
-	private String fullName;
+	private String firstName;
+	private String lastName;
 	private String email;
 	private String password;
-	public String getFullName() {
-		return fullName;
+	
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 	public String getEmail() {
 		return email;
