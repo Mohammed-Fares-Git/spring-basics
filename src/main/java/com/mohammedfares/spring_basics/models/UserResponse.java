d58202a617302e02e69c905ac65cea5f732ad5a2
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-23 12:24:14 +0100

    create the UserResponse

--- src/main/java/com/mohammedfares/spring_basics/models/UserResponse.java
+++ src/main/java/com/mohammedfares/spring_basics/models/UserResponse.java
@@ -2,13 +2,27 @@ package com.mohammedfares.spring_basics.models;
 
 public class UserResponse {
 	
-	private String fullName;
+	private String userId;
+	private String firstName;
+	private String lastName;
 	private String email;
-	public String getFullName() {
-		return fullName;
+	public String getUserId() {
+		return userId;
 	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 	public String getEmail() {
 		return email;
@@ -16,6 +30,7 @@ public class UserResponse {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	
 	
 
